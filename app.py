ad8c3193e84ec7122f7a4024771a41a3ba54a27a
hNandrajog <5nkharpreet@gmail.com>
2025-06-29 16:24:11 +0530

    Fixed port for render app.py

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def ping():
     return "Server is up!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
